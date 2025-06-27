f34567d1523edc82cbce6c64fc60c2c6b2ab0ca8
isGoodSoup <sargent.pointed453@passinbox.com>
2025-06-27 17:17:46 +0200

    ProjectJava

---
+++ java/ProyectoJava/src/HolaMundo.java
@@ -0,0 +1,11 @@
+public class HolaMundo {
+    
+    public static void main( String[] args ) {
+        System.out.println("Hola Mundo");
+
+        int edad = 21;
+        int numero = 10;
+
+        System.out.println("Resultado: " + edad + numero);
+    }
+}
