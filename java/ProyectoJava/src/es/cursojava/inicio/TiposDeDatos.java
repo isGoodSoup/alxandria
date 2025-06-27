64397be507770f49177cfa230e02c0b7f58a5b99
isGoodSoup <sargent.pointed453@passinbox.com>
2025-06-27 17:46:15 +0200

    ProyectoJava

---
+++ java/ProyectoJava/src/es/cursojava/inicio/TiposDeDatos.java
@@ -0,0 +1,30 @@
+public class TiposDeDatos {
+    
+    public static void main( String[] args ) {
+        // Tipos primitivos 
+        // Enteros
+        /*
+         * byte  8   -128    127
+         * short 16  -32.768 32.767
+         * int   32  32      2³¹
+         * long  64  -2⁶³    2⁶³-1
+         */
+
+        byte edad = 10;
+        short numero2 = 123;
+        int numero = 2300;
+        long numero3 = 1890;
+
+        // Decimales
+        double decimal1 = 9.7;
+        float decimal2 = 100.9f;
+
+        // Caracteres
+        char caracter = 'a';
+        boolean esMayorEdad = false;
+        System.out.println(esMayorEdad);
+        
+
+        // Tipos objeto
+    }
+}
