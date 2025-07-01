464e680440de0762b1852a61da73bf2ff9342b74
isGoodSoup <sargent.pointed453@passinbox.com>
2025-07-01 18:31:22 +0200

    feat: progress

--- java/ProyectoJava/src/es/cursojava/inicio/condicionales/ejercicios/MenuOpciones.java
+++ java/ProyectoJava/src/es/cursojava/inicio/condicionales/ejercicios/MenuOpciones.java
@@ -14,26 +14,36 @@ public class MenuOpciones {
 		System.out.println("3-- Añadir Alumno");
 		System.out.println("4-- Salir");
 		
+		System.out.println("--------------------------------");
 		int response = scan.nextInt();
 		
-		if (response == 1) {
-			System.out.println("Pintando cuadrado...");
-			System.out.println("-----");
-			System.out.println("|    |");
-			System.out.println("|    |");
-			System.out.println("-----");
-		} else if (response == 2) {
-			System.out.println("Validando email...");
-		} else if (response == 3) {
+//		if (response == 1) {
+//			System.out.println("Pintando cuadrado...");
+//			System.out.println("-----");
+//			System.out.println("|    |");
+//			System.out.println("|    |");
+//			System.out.println("-----");
+//		} else if (response == 2) {
+//			System.out.println("Validando email...");
+//		} else if (response == 3) {
 			
-			System.out.println("Añadir nuevo alumno:");
-			String alumnoNuevo = scan.nextLine();
-			System.out.println("Nuevo alumno registrado: " + alumnoNuevo);
+//			System.out.println("Añadir nuevo alumno:");
+//			String alumnoNuevo = scan.nextLine();
+//			System.out.println("Nuevo alumno registrado: " + alumnoNuevo);
 			
-		} else if (response == 4) {
-			System.out.println("Saliendo...");
-		} else {
-			System.out.println("Respuesta no válida.");
+//		} else if (response == 4) {
+//			System.out.println("Saliendo...");
+//		} else {
+//			System.out.println("Respuesta no válida.");
+//		}
+
+		switch (response) {
+			case 1 : System.out.println("Pintando cuadrado... □"); break;
+			case 2 : System.out.println("Validando email."); break;
+			case 3 : System.out.println("Nuevo alumno"); 
+					 String alumnoNuevo = scan.nextLine(); 
+					 System.out.println("Nuevo alumno [" + alumnoNuevo + "] registrado."); break; 
+			case 4 : System.out.println("Saliendo"); break;
 		}
 	}
 }
