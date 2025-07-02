59f4f7adf5ae1de9d612f1df1ee17f35de2753be
isGoodSoup <microsoft.delusion983@passinbox.com>
2025-07-03 00:16:15 +0200

    style: spaciados

--- java/ProyectoJava/src/es/cursojava/inicio/bucles/ejercicios/CollatzConjentura.java
+++ java/ProyectoJava/src/es/cursojava/inicio/bucles/ejercicios/CollatzConjentura.java
@@ -1,5 +1,25 @@
 package bucles.ejercicios;
 
+import java.util.Scanner;
+
 public class CollatzConjentura {
 
+    public static void main( String[] args ) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Inserta el número en cuestión:");
+        int numero = scan.nextInt();
+
+        while (numero != 1) { 
+            System.out.println(numero);
+
+            if (numero%2==0) {
+                numero /= 2;
+            } else {
+                numero *= 3;
+                numero += 1;
+            }
+        }
+        System.out.println("Resultado final: " + numero);
+    }
 }
