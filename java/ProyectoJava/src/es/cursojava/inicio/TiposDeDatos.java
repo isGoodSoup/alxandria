2e7ea594497848f060087dd0d1191b258184f0a1
isGoodSoup <sargent.pointed453@passinbox.com>
2025-07-02 17:27:29 +0200

    fix: resource leak

--- java/ProyectoJava/src/es/cursojava/inicio/TiposDeDatos.java
+++ java/ProyectoJava/src/es/cursojava/inicio/TiposDeDatos.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class TiposDeDatos {
     
+    @SuppressWarnings("resource")
     public static void main( String[] args ) {
         // Tipos primitivos 
         // Enteros
