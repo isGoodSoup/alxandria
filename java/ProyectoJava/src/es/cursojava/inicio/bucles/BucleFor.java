3647985dc85da740a23665d0fc123a12901143a7
isGoodSoup <sargent.pointed453@passinbox.com>
2025-07-02 17:48:32 +0200

    feat: bucle "for"

---
+++ java/ProyectoJava/src/es/cursojava/inicio/bucles/BucleFor.java
@@ -0,0 +1,11 @@
+package bucles;
+
+public class BucleFor {
+
+    public static void main(String[] args) {
+//        Bucle infinito
+//        for ( ; ; ) {
+//            System.out.println("Hello World!");
+//        }
+    }
+}
