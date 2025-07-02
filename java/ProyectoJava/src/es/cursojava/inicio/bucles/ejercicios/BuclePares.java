889650b8e2151c0cfe931e8a008117718b494e9f
isGoodSoup <sargent.pointed453@passinbox.com>
2025-07-02 20:26:40 +0200

    feat: bucle "while", ejercicio 1

---
+++ java/ProyectoJava/src/es/cursojava/inicio/bucles/ejercicios/BuclePares.java
@@ -0,0 +1,11 @@
+package bucles.ejercicios;
+
+public class BuclePares {
+
+    public static void main(String[] args) {
+        
+        for (int i = 0, x = 99; i <= 100; i += 2, x -= 2) {
+            System.out.println(i + ", " + x);
+        }
+    }
+}
