ddd36f97c31f172d4ebe1123fe8c73f83e585ad3
isGoodSoup <microsoft.delusion983@passinbox.com>
2025-07-03 23:05:32 +0200

    feat: bucle (while, for), ejercicio 9

--- java/ProyectoJava/src/es/cursojava/inicio/bucles/ejercicios/BucleMenu.java
+++ java/ProyectoJava/src/es/cursojava/inicio/bucles/ejercicios/BucleMenu.java
@@ -18,42 +18,46 @@ public class BucleMenu {
 		int opcion = 1;
 
         switch (opcion) {
-            case 1: System.out.println("Pintando cuadrado...");
-                    System.out.println("Tamaño del cuadrado?");
-                    int width = scan.nextInt();
-                    System.out.println("Tamaño seleccionado: " + width);
-
-                    System.out.println();
-
-                    boolean isPrinterActive = true;
-                    int count = 0;
-                    String cell = "*";
-
-                    while (isPrinterActive) {
-
-                        if (count == width) {
-                            break;
-                        }
-                        
-                        count += 1;
-                        for (int i = 0; i < width; i++) {
-                            System.out.print(cell);
-
-                            
-                        }
-                        System.out.println();
+            case 1 -> {
+                System.out.println("Pintando cuadrado...");
+                System.out.println("Tamaño del cuadrado?");
+                int width = scan.nextInt();
+                System.out.println("Tamaño seleccionado: " + width);
+                
+                System.out.println();
+                
+                boolean isPrinterActive = true;
+                int count = 0;
+                String cell = "*";
+                
+                while (isPrinterActive) {
+                    
+                    if (count == width) {
+                        break;
                     }
-
-                    break;
-            case 2: System.out.println("[] Validando email...");
-                    System.out.println("Email entrado ya validado.");
-                    break;
-            case 3: System.out.println("Añadir alumno: ");
-                    String alumno = scan.nextLine();
-                    System.out.println(alumno + " queda registrado.");
-                    break;
-            case 4: System.out.println("Saliendo"); break;
-            default: System.out.println("Operación inválida."); break;
+                    
+                    count += 1;
+                    for (int i = 0; i < width; i++) {
+                        if (count == 1 || count == width || i == 0 || i == width - 1) {
+                            System.out.print(cell);
+                        } else {
+                            System.out.print(" ");
+                        }
+                    }
+                    System.out.println();
+                }
+            }
+            case 2 -> {
+                System.out.println("[] Validando email...");
+                System.out.println("Email entrado ya validado.");
+            }
+            case 3 -> {
+                System.out.println("Añadir alumno: ");
+                String alumno = scan.nextLine();
+                System.out.println(alumno + " queda registrado.");
+            }
+            case 4 -> System.out.println("Saliendo");
+            default -> System.out.println("Operación inválida.");
         }
     }
 }
