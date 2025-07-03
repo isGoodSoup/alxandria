b263dccbcaaa664c86c7a173a5cf431d5336b0cb
isGoodSoup <sargent.pointed453@passinbox.com>
2025-07-03 18:25:56 +0200

    feat: corregido ejercicio 4

--- java/ProyectoJava/src/es/cursojava/inicio/bucles/ejercicios/TablasMultiplicar.java
+++ java/ProyectoJava/src/es/cursojava/inicio/bucles/ejercicios/TablasMultiplicar.java
@@ -17,8 +17,15 @@ public class TablasMultiplicar {
         while (multiplosActivos) {
             count += 1;
 
-            for (int i = 1; i < 11; i++) {
-                System.out.println(count + " x " + i + " = " + count*i);
+            for (int i = 1; i <= numero; i++) {
+
+                System.out.println("Tabla del " + i);
+
+                for (int j = 1; j <= 10; j++) {
+                    System.out.println(i + " x " + j + " = " + i*j);
+                }
+
+                System.out.println();
             }
 
             if (count==numero) {
