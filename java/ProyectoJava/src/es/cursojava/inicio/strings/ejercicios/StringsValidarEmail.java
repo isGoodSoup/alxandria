d16488d891b267fd51a83051db2f793ad4ed22d6
isGoodSoup <sargent.pointed453@passinbox.com>
2025-07-10 23:13:33 +0200

    feat: strings, ejercicio 5

--- java/ProyectoJava/src/es/cursojava/inicio/strings/ejercicios/StringsValidarEmail.java
+++ java/ProyectoJava/src/es/cursojava/inicio/strings/ejercicios/StringsValidarEmail.java
@@ -7,37 +7,48 @@ public class StringsValidarEmail {
 	public static void main( String[] args ) {
 		
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Introduce tu email:"); System.out.print("-- ");
-		String email = scan.nextLine();
-		int atCount = 0;
 		
-		for (int i = 0; i < email.length(); i++) {
-			char ch = email.charAt(i);
+		int atCount = 0;
+		boolean isThereAt = false, isThereSpaces = false, isThereDot = false;
+
+		do {
+			System.out.println("Introduce tu email:"); System.out.print("-- ");
+			String email = scan.nextLine();
+
+			for (int i = 0; i < email.length(); i++) {
 				
-			if (ch == '@') {
-				
-				if (atCount > 1) {
-					System.out.println("(-) Contiene más de una @");
+				char ch = email.charAt(i);
+
+				if (ch == ' ') {
+
+					isThereSpaces = true;
+					System.out.println("- Contiene espacios en blanco");
 					break;
 				}
-				
-				System.out.println("(+) Contiene una @");
-				atCount++;
-				
-				boolean dotPosition = email.substring(i, i + 2).contains(".");
-				
-				if (dotPosition) {
-					System.out.println("(+) Contiene al menos un punto a dos caracteres");
-				} else if (!dotPosition){
-					System.out.println("(-) NO contiene el punto donde debe contenerlo");
-					break;
+			}	
+
+			for (int i = 0; i < email.length(); i++) {
+
+				if (email.charAt(i) == '@') {
+					atCount++;
 				}
 			}
-				
-			if (ch == ' ') {
-				System.out.println("(-) No debe contener espacios en blanco");
-				break;
+
+			if (atCount == 1) {
+				System.out.println("+ Contiene una arroba");
+				isThereAt = true;
+			} else {
+				System.out.println("- Contiene más de una arroba");
 			}
-		}
+
+			for (int i = 0; i < email.length(); i++) {
+
+				if (email.contains(".")) {
+					
+				}
+
+			}
+
+		} while (isThereSpaces || !isThereAt || isThereDot);
 	}
 }
